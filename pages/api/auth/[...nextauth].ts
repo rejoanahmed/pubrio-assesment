545ea9b3f3c0a3945ef24be8187e30eb8047db17
rejoanahmed <rejoanahmed8@gmail.com>
2023-03-20 23:55:08 +0800

    dangerous log

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,8 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
+console.log(process.env.GOOGLE_CLIENT_ID)
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
