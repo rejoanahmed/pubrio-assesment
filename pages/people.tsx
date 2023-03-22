9818ab650d1089e6618cf3c9d5807dc7af51192b
rejoanahmed <rejoanahmed8@gmail.com>
2023-03-22 10:25:38 +0800

    styling accordion

--- pages/people.tsx
+++ pages/people.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { SyntheticEvent, useState } from 'react'
 import PrimaryLayout from '../components/layout/PrimaryLayout'
 import SecondaryLayout, { SearchNavItems } from '../components/layout/Secondary'
 import { NextPageWithLayout } from './_app'
@@ -10,18 +10,11 @@ import MuiAccordionSummary, {
 } from '@mui/material/AccordionSummary'
 import MuiAccordionDetails from '@mui/material/AccordionDetails'
 import Typography from '@mui/material/Typography'
+import SearchIcon from '@mui/icons-material/Search'
 
 const Accordion = styled((props: AccordionProps) => (
-  <MuiAccordion disableGutters elevation={0} square {...props} />
-))(({ theme }) => ({
-  border: `1px solid ${theme.palette.divider}`,
-  '&:not(:last-child)': {
-    borderBottom: 0
-  },
-  '&:before': {
-    display: 'none'
-  }
-}))
+  <MuiAccordion disableGutters elevation={0} {...props} />
+))(({ theme }) => ({}))
 
 const AccordionSummary = styled((props: AccordionSummaryProps) => (
   <MuiAccordionSummary
@@ -47,17 +40,57 @@ const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
 }))
 
 const People: NextPageWithLayout = () => {
-  const [expanded, setExpanded] = React.useState<number | false>(false)
+  const [expanded, setExpanded] = useState<number | false>(false)
 
   const handleChange =
-    (panel: number) => (event: React.SyntheticEvent, newExpanded: boolean) => {
+    (panel: number) => (event: SyntheticEvent, newExpanded: boolean) => {
       setExpanded(newExpanded ? panel : false)
     }
 
+  const [inputValue, setInputValue] = useState('')
+  const [searchActive, setSearchActive] = useState(false)
+
   return (
-    <div className='w-96 h-[calc(100vh-90px)] overflow-scroll'>
+    <div className='w-96 h-[calc(100vh-90px)] overflow-scroll border-r-2 shadow-2xl relative'>
+      <div className='sticky top-0 z-10 px-8 py-4 backdrop-blur-sm shadow-lg'>
+        <div
+          className={` flex items-center border ${
+            searchActive ? 'border-blue-400 ' : ''
+          }rounded px-2 justify-center`}
+          style={{
+            width: 200
+          }}
+        >
+          <SearchIcon
+            sx={{ color: searchActive ? 'rgb(96 165 250)' : 'rgba(0,0,0,.4)' }}
+          />
+
+          <input
+            className='w-full block outline-none h-10 ml-2 bg-transparent'
+            type='text'
+            name='topSearch'
+            id='topSearch'
+            placeholder='Search'
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            onFocus={() => {
+              !searchActive && setSearchActive(true)
+            }}
+            onBlur={() => {
+              !inputValue && setSearchActive(false)
+            }}
+          />
+        </div>
+      </div>
+      <h1 className='px-8 mt-8 mb-3 font-bold text-lg'>Filters</h1>
       {Array.from(new Array(30)).map((_, index) => (
-        <Accordion expanded={expanded === index} onChange={handleChange(index)}>
+        <Accordion
+          expanded={expanded === index}
+          onChange={handleChange(index)}
+          sx={{
+            border: `2px solid ${expanded === index && 'blue'}`
+          }}
+        >
           <AccordionSummary
             aria-controls={`panel${index}d-header`}
             id={`panel${index}d-header`}
